e8954c5f8208a6af7efa80a90a0eba281b584d78
khatri-raj <rajkhatri8060@gmail.com>
2025-05-12 18:50:33 +0530

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Course
 
+def home(request):
+    return render(request, 'home.html')
+
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'course_list.html', {'courses': courses})
+
+
+def review(request):
+    return render(request, 'review.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def help(request):
+    return render(request, 'help.html')

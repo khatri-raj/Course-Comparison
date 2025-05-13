b92a3bcf2422b3fba4460fd5032e8e1f9466251c
khatri-raj <rajkhatri8060@gmail.com>
2025-05-13 09:07:49 +0530

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -10,3 +10,14 @@ class Course(models.Model):
     Syllabus = models.TextField()
     def __str__(self):
         return self.Name
+    
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200, blank=True)
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.subject or 'No Subject'}"

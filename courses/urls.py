139f4be9ff8073a87cd238bb65fc8d5952cb5532
khatri-raj <rajkhatri8060@gmail.com>
2025-05-14 14:38:40 +0530

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,8 @@ from .import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('Compare/', views.course_list, name='course_list'),
-    path('review', views.review, name="review"),
-    path('contact',views.contact, name="contact"),
-    path('help', views.help, name="help"),
+    path('review/', views.review, name="review"),
+    path('contact/',views.contact, name="contact"),
+    path('help/', views.help, name="help"),
+    path('reviews_list/', views.reviews_list, name="reviews_list"),
 ]

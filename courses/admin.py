89932e941ccd56d88724fa2c0b474963cefcb9e9
khatri-raj <rajkhatri8060@gmail.com>
2025-05-14 14:38:04 +0530

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Course,ContactMessage
+from .models import Course,ContactMessage,Review
 
 admin.site.register(Course)
 admin.site.register(ContactMessage)
+admin.site.register(Review)

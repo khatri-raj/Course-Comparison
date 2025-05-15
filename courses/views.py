845d5325466ab79878f2959f4fd21f86156894a1
khatri-raj <rajkhatri8060@gmail.com>
2025-05-15 13:29:01 +0530

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -2,31 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Course, Review
 from .forms import ContactMessageForm
 
-
 def home(request):
     return render(request, 'home.html')
 
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'course_list.html', {'courses': courses})
-
-# def review(request):
-#     # Get all the courses to display in the form
-#     courses = Course.objects.all()
-#     # Get all reviews to display
-#     reviews = Review.objects.all()
-
-#     if request.method == "POST":
-#         # Create a new review based on the form data
-#         course_id = request.POST.get('course')
-#         rating = float(request.POST.get('rating'))
-#         comment = request.POST.get('comment')
-#         # Save the review to the database
-#         course = Course.objects.get(id=course_id)
-#         Review.objects.create(course=course, rating=rating, comment=comment)
-#         # Redirect to the same page after submitting the review
-#         return redirect('reviews_list')
-#     return render(request, 'review.html', {'courses': courses, 'reviews': reviews})
 
 def contact(request):
     if request.method == 'POST':
@@ -62,8 +43,3 @@ def review(request):
     # Show review list
     reviews = Review.objects.all()
     return render(request, 'review.html', {'reviews': reviews})
-
-# def reviews_list(request):
-#     # Get all reviews to display
-#     reviews = Review.objects.all()
-#     return render(request, 'reviews_list.html', {'reviews': reviews})
